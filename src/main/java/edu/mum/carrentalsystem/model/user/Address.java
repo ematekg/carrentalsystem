6c24aa40c6460d855085918393e3ea749470ca33
Merih <merimemax@gmail.com>
2018-06-18 08:34:57 -0500

    gitIgnore file updates

--- src/main/java/edu/mum/carrentalsystem/model/user/Address.java
+++ src/main/java/edu/mum/carrentalsystem/model/user/Address.java
@@ -1,12 +1,62 @@
 package edu.mum.carrentalsystem.model.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Address {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String street;
     private String city;
     private String state;
     private int zipCode;
 
+    public Address() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
 }
 
