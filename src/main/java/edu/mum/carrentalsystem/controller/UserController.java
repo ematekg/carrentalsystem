415f544903e470e38de098922d38887bc26d48d1
ematekg <ematekg@yahoo.com>
2018-06-20 15:54:22 -0700

    updated return a vehicle subystem

--- src/main/java/edu/mum/carrentalsystem/controller/UserController.java
+++ src/main/java/edu/mum/carrentalsystem/controller/UserController.java
@@ -1,6 +1,7 @@
 package edu.mum.carrentalsystem.controller;
 
 import edu.mum.carrentalsystem.model.payment.PaymentInfo;
+import edu.mum.carrentalsystem.model.rent.Promo;
 import edu.mum.carrentalsystem.model.rent.RentRecord;
 import edu.mum.carrentalsystem.model.user.*;
 import edu.mum.carrentalsystem.model.vehicle.Vehicle;
@@ -8,10 +9,10 @@ import edu.mum.carrentalsystem.service.userHandler.ICustomerDetailsHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import javax.xml.transform.sax.SAXSource;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -37,13 +38,31 @@ public class UserController {
 
         RentRecord rentRecord=customer.getRentrecord().get(0);
 
-        model.addAttribute("customer",customer);
+       // model.addAttribute("customer",customer);
         model.addAttribute("rentrecord",rentRecord);
 
         return "admin/return";
 
     }
 
+
+
+    @RequestMapping(value="/promocode", method=RequestMethod.POST)
+    public String addPromodiscount(@ModelAttribute("code") String code,@ModelAttribute("custid") long id,Model model) {
+        Promo promo=new Promo("123xyz",5.0);
+        Customer customer=customerDetailsHandler.findOne(id);
+
+        RentRecord rentRecord=customer.getRentrecord().get(0);
+        if(promo.getCode().equals(code)){
+
+            rentRecord.setPromo(promo);
+            model.addAttribute("rentrecord",rentRecord);
+
+            return "admin/return";
+        }
+
+         return "redirect:/return/" +  rentRecord.getCustomer().getUserId();
+    }
 
 
 
